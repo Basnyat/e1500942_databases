ae6d8dfec8e2927cf87bb88548e72a7251eed80f
unknown <e1500942@WA2029-03.ad.puv.fi>
2017-02-15 14:21:46 +0200

    first commit

---
+++ src/assignment1/Example1.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class Example1 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Your name?");
+		String name = input.nextLine();
+		System.out.println("Hello" + name);
+		input.close();
+	}
+	
+	
+
+}
